f1f54f1e9ec09558cc3817629386112c0ba1a21b
Harry <harryl@metropolia.fi>
2025-11-06 23:00:37 +0200

    MODULE 1

--- MODULE_11/assignment_11_1.py
+++ MODULE_11/assignment_11_1.py
@@ -3,21 +3,31 @@ class Publication:
         self.name = name
 
 
-class Book:
-    def __init__(self, author, page_count):
+class Book(Publication):
+    def __init__(self,name, author, page_count):
         self.author = author
         self.page_count = page_count
         super().__init__(name)
 
 
     def print_information(self):
+        print(f" name of publication: {self.name} name of author : {self.author} , page count: {self.page_count}")
 
 
 
-class Magazine:
-    def __init__(self, chief_editor):
+class Magazine(Publication):
+    def __init__(self, name, chief_editor):
         self.chief_editor = chief_editor
         super().__init__(name)
 
 
     def print_information(self):
+        print(f"name of publication is : {self.name}")
+        print(f"name of chief editor is: {self.chief_editor}")
+
+
+teos=Magazine("Donald Duck","Aki Hyyppä")
+teos1=Book("Compartment No.6", "Rosa Liksom", 192)
+teos.print_information()
+teos1.print_information()
+
